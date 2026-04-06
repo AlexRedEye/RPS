3527bfa9cc739b61cb44c81b4dd8c40b3627b863
Alex <alex.business@pocketfriends.org>
2026-04-06 12:48:56 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("1. Rock")
+    print("2. Paper")
+    print("3. Scissors")
+    choice = input()
+
+    match choice:
+        case "1":
+            print("You chose rock")
+
+main()
