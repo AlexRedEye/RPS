7ccb13712df50cb80ea1372976453aa343c6fd04
Alex <alex.business@pocketfriends.org>
2026-04-07 12:35:29 -0400

    Add loop

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def main():
-    print("1. Rock")
-    print("2. Paper")
-    print("3. Scissors")
-    choice = input()
+    while True:
+        print("1. Rock")
+        print("2. Paper")
+        print("3. Scissors")
+        choice = input()
 
-    match choice:
-        case "1":
-            print("You chose rock")
-        case "2":
-            print("You chose paper")
-        case "3":
-            print("You chose scissors")
-        case _:
-            print("Not a valid choice")
+        match choice:
+            case "1":
+                print("You chose rock")
+            case "2":
+                print("You chose paper")
+            case "3":
+                print("You chose scissors")
+            case _:
+                print("Not a valid choice")
 
 main()
