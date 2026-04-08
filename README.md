583a804d452b702610743e3a5586236b5b0c2191
Alex <alex.business@pocketfriends.org>
2026-04-08 12:50:28 -0400

    Add stat tracker

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 simple rock paper scissors with random ai choice
+a win/loss/tie stat tracker
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import random
 
 def main():
     options = ["Rock", "Paper", "Scissors"]
+    wins = 0
+    losses = 0
+    ties = 0
+
     while True:
+        print(f"Wins: {wins} | Losses: {losses} | Ties: {ties}")
         for option in options:
             print(f"{options.index(option) + 1}: {option}")
         choice = input()
@@ -14,24 +19,33 @@ def main():
             case "1":
                 if ai_choice == "Paper":
                     print("Ai chose paper you lose")
+                    losses += 1
                 elif ai_choice == "Scissors":
                     print("Ai chose scissors you win")
+                    wins += 1
                 else:
                     print("Its a tie")
+                    ties += 1
             case "2":
                 if ai_choice == "Scissors":
                     print("Ai chose Scissors you lose")
+                    losses += 1
                 elif ai_choice == "Rock":
                     print("Ai chose scissors you win")
+                    wins += 1
                 else:
                     print("Its a tie")
+                    ties += 1
             case "3":
                 if ai_choice == "Rock":
                     print("Ai chose Rock you lose")
+                    losses += 1
                 elif ai_choice == "Paper":
                     print("Ai chose paper you win")
+                    wins += 1
                 else:
                     print("Its a tie")
+                    ties += 1
             case _:
                 print("Not a valid choice")
 
